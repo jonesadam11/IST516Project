e92e88ffdc4721b91409329369c5d1b46c67d123
jonesadam11 <jones.adam11@gmail.com>
2013-11-16 13:26:40 -0800

    Web Links Added to Buttons

--- src/com/example/pennstatehub/MainActivity.java
+++ src/com/example/pennstatehub/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.pennstatehub;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.content.Intent;
+import android.view.View;
+import android.net.Uri;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +13,27 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    
+    public void goToWebmail(View view)
+    {
+    	goToUrl("https://m.webmail.psu.edu/webmail/index.cgi");
+    }
+    
+    public void goToPSU(View view)
+    {
+    	goToUrl("http://m.psu.edu");
+    }
+    
+    public void goToCatalog(View view)
+    {
+    	goToUrl("http://m.psu.edu/soc/");
+    }
+    
+    private void goToUrl (String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
     }
 
 
